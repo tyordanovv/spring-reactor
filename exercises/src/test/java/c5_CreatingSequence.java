0fb543bbf259a59c04671c79ad52593d8d208866
Tihomir Yordanov <t.yordanovv@gmail.com>
2022-12-20 20:15:38 +0200

    first 10

---
+++ exercises/src/test/java/c5_CreatingSequence.java
@@ -0,0 +1,312 @@
+import org.junit.jupiter.api.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+/**
+ * In this chapter we are going to cover fundamentals of how to create a sequence. At the end of this
+ * chapter we will tackle more complex methods like generate, create, push, and we will meet them again in following
+ * chapters like Sinks and Backpressure.
+ * Read first:
+ * https://projectreactor.io/docs/core/release/reference/#which.create
+ * https://projectreactor.io/docs/core/release/reference/#producing
+ * https://projectreactor.io/docs/core/release/reference/#_simple_ways_to_create_a_flux_or_mono_and_subscribe_to_it
+ * Useful documentation:
+ * https://projectreactor.io/docs/core/release/reference/#which-operator
+ * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
+ * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
+ *
+ * @author Stefan Dragisic
+ */
+public class c5_CreatingSequence {
+
+    /**
+     * Emit value that you already have.
+     */
+    @Test
+    public void value_I_already_have_mono() {
+        String valueIAlreadyHave = "value";
+        Mono<String> valueIAlreadyHaveMono = Mono.just(valueIAlreadyHave);
+
+        StepVerifier.create(valueIAlreadyHaveMono)
+                    .expectNext("value")
+                    .verifyComplete();
+    }
+
+    /**
+     * Emit potentially null value that you already have.
+     */
+    @Test
+    public void potentially_null_mono() {
+        String potentiallyNull = null;
+        Mono<String> potentiallyNullMono = Mono.justOrEmpty(potentiallyNull);
+
+        StepVerifier.create(potentiallyNullMono)
+                    .verifyComplete();
+    }
+
+    /**
+     * Emit value from a optional.
+     */
+    @Test
+    public void optional_value() {
+        Optional<String> optionalValue = Optional.of("optional");
+        Mono<String> optionalMono = Mono.justOrEmpty(optionalValue);
+
+        StepVerifier.create(optionalMono)
+                    .expectNext("optional")
+                    .verifyComplete();
+    }
+
+    /**
+     * Convert callable task to Mono.
+     */
+    @Test
+    public void callable_counter() {
+        AtomicInteger callableCounter = new AtomicInteger(0);
+        Callable<Integer> callable = () -> {
+            System.out.println("You are incrementing a counter via Callable!");
+            return callableCounter.incrementAndGet();
+        };
+
+        Mono<Integer> callableCounterMono = Mono.fromCallable(callable); //todo: change this line only
+
+        StepVerifier.create(callableCounterMono.repeat(2))
+                    .expectNext(1, 2, 3)
+                    .verifyComplete();
+    }
+
+    /**
+     * Convert Future task to Mono.
+     */
+    @Test
+    public void future_counter() {
+        AtomicInteger futureCounter = new AtomicInteger(0);
+        CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> {
+            System.out.println("You are incrementing a counter via Future!");
+            return futureCounter.incrementAndGet();
+        });
+        Mono<Integer> futureCounterMono = Mono.fromFuture(completableFuture);
+
+        StepVerifier.create(futureCounterMono)
+                    .expectNext(1)
+                    .verifyComplete();
+    }
+
+    /**
+     * Convert Runnable task to Mono.
+     */
+    @Test
+    public void runnable_counter() {
+        AtomicInteger runnableCounter = new AtomicInteger(0);
+        Runnable runnable = () -> {
+            runnableCounter.incrementAndGet();
+            System.out.println("You are incrementing a counter via Runnable!");
+        };
+        Mono<Integer> runnableMono = Mono.fromRunnable(runnable);
+
+        StepVerifier.create(runnableMono.repeat(2))
+                    .verifyComplete();
+
+        Assertions.assertEquals(3, runnableCounter.get());
+    }
+
+    /**
+     * Create Mono that emits no value but completes successfully.
+     */
+    @Test
+    public void acknowledged() {
+        Mono<String> acknowledged = Mono.empty();
+
+        StepVerifier.create(acknowledged)
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Mono that emits no value and never completes.
+     */
+    @Test
+    public void seen() {
+        Mono<String> seen = Mono.never();
+
+        StepVerifier.create(seen.timeout(Duration.ofSeconds(5)))
+                    .expectSubscription()
+                    .expectNoEvent(Duration.ofSeconds(4))
+                    .verifyTimeout(Duration.ofSeconds(5));
+    }
+
+    /**
+     * Create Mono that completes exceptionally with exception `IllegalStateException`.
+     */
+    @Test
+    public void trouble_maker() {
+        Mono<String> trouble = Mono.error(new IllegalStateException());
+
+        StepVerifier.create(trouble)
+                    .expectError(IllegalStateException.class)
+                    .verify();
+    }
+
+    /**
+     * Create Flux that will emit all values from the array.
+     */
+    @Test
+    public void from_array() {
+        Integer[] array = {1, 2, 3, 4, 5};
+        Flux<Integer> arrayFlux = Flux.fromArray(array);
+
+        StepVerifier.create(arrayFlux)
+                    .expectNext(1, 2, 3, 4, 5)
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Flux that will emit all values from the list.
+     */
+    @Test
+    public void from_list() {
+        List<String> list = Arrays.asList("1", "2", "3", "4", "5");
+        Flux<String> listFlux = Flux.fromIterable(list);
+
+        StepVerifier.create(listFlux)
+                    .expectNext("1", "2", "3", "4", "5")
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Flux that will emit all values from the steam.
+     */
+    @Test
+    public void from_stream() {
+        Stream<String> stream = Stream.of("5", "6", "7", "8", "9");
+        Flux<String> streamFlux = Flux.fromStream(stream);
+
+        StepVerifier.create(streamFlux)
+                    .expectNext("5", "6", "7", "8", "9")
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Flux that emits number incrementing numbers at interval of 1 second.
+     */
+    @Test
+    public void interval() {
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+
+        System.out.println("Interval: ");
+        StepVerifier.create(interval.take(3).doOnNext(System.out::println))
+                    .expectSubscription()
+                    .expectNext(0L)
+                    .expectNoEvent(Duration.ofMillis(900))
+                    .expectNext(1L)
+                    .expectNoEvent(Duration.ofMillis(900))
+                    .expectNext(2L)
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Flux that emits range of integers from [-5,5].
+     */
+    @Test
+    public void range() {
+        Flux<Integer> range = Flux.range(-5,11);
+
+        System.out.println("Range: ");
+        StepVerifier.create(range.doOnNext(System.out::println))
+                    .expectNext(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5)
+                    .verifyComplete();
+    }
+
+    /**
+     * Create Callable that increments the counter and returns the counter value, and then use `repeat()` operator to create Flux that emits
+     * values from 0 to 10.
+     */
+    @Test
+    public void repeat() {
+        AtomicInteger counter = new AtomicInteger(0);
+        Flux<Integer> repeated = Mono
+                .fromCallable(counter::incrementAndGet)
+                .repeat(9);
+
+        System.out.println("Repeat: ");
+        StepVerifier.create(repeated.doOnNext(System.out::println))
+                    .expectNext(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                    .verifyComplete();
+    }
+
+    /**
+     * Following example is just a basic usage of `generate,`create`,`push` sinks. We will learn how to use them in a
+     * more complex scenarios when we tackle backpressure.
+     * Answer:
+     * - What is difference between `generate` and `create`? -> synchronous vs asynchronous
+     * - What is difference between `create` and `push`?
+     */
+    @Test
+    public void generate_programmatically() {
+        AtomicInteger atomicCounter = new AtomicInteger(1);
+
+        Flux<Integer> generateFlux = Flux.generate(sink -> {
+            if (atomicCounter.get() > 5) sink.complete();
+            sink.next(atomicCounter.getAndIncrement());
+        });
+
+        //------------------------------------------------------
+
+        Flux<Integer> createFlux = Flux.create(sink -> {
+            for (int i = 1; i <=5; i++){
+                sink.next(i);
+            }
+            sink.complete();
+        });
+
+        //------------------------------------------------------
+
+        Flux<Integer> pushFlux = Flux.push(sink -> {
+            for (int i = 1; i <=5; i++){
+                sink.next(i);
+            }
+            sink.complete();
+        });
+
+        StepVerifier.create(generateFlux)
+                    .expectNext(1, 2, 3, 4, 5)
+                    .verifyComplete();
+
+        StepVerifier.create(createFlux)
+                    .expectNext(1, 2, 3, 4, 5)
+                    .verifyComplete();
+
+        StepVerifier.create(pushFlux)
+                    .expectNext(1, 2, 3, 4, 5)
+                    .verifyComplete();
+    }
+
+    /**
+     * Something is wrong with the following code. Find the bug and fix it so test passes.
+     */
+    @Test
+    public void multi_threaded_producer() {
+        Flux<Integer> producer = Flux.create(sink -> {
+            for (int i = 0; i < 100; i++) {
+                int finalI = i;
+                new Thread(() -> sink.next(finalI)).start(); //don't change this line!
+            }
+        });
+
+        //do not change code below
+        StepVerifier.create(producer
+                                    .doOnNext(System.out::println)
+                                    .take(100))
+                    .expectNextCount(100)
+                    .verifyComplete();
+    }
+}
---
+++ exercises/src/test/java/c6_CombiningPublishers.java
@@ -0,0 +1,357 @@
+import org.junit.jupiter.api.*;
+import reactor.blockhound.BlockHound;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Hooks;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+
+/**
+ * In this important chapter we are going to cover different ways of combining publishers.
+ *
+ * Read first:
+ *
+ * https://projectreactor.io/docs/core/release/reference/#which.values
+ *
+ * Useful documentation:
+ *
+ * https://projectreactor.io/docs/core/release/reference/#which-operator
+ * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
+ * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
+ *
+ * @author Stefan Dragisic
+ */
+public class c6_CombiningPublishers extends CombiningPublishersBase {
+
+    /**
+     * Goal of this exercise is to retrieve e-mail of currently logged-in user.
+     * `getCurrentUser()` method retrieves currently logged-in user
+     * and `getUserEmail()` will return e-mail for given user.
+     *
+     * No blocking operators, no subscribe operator!
+     * You may only use `flatMap()` operator.
+     */
+    @Test
+    public void behold_flatmap() {
+        Hooks.enableContextLossTracking();
+
+        Mono<String> currentUserEmail = getCurrentUser()
+                .flatMap(this::getUserEmail);
+
+        //don't change below this line
+        StepVerifier.create(currentUserEmail)
+                    .expectNext("user123@gmail.com")
+                    .verifyComplete();
+    }
+
+    /**
+     * `taskExecutor()` returns tasks that should execute important work.
+     * Get all the tasks and execute them.
+     *
+     * Answer:
+     * - Is there a difference between Mono.flatMap() and Flux.flatMap()?
+     */
+    @Test
+    public void task_executor() {
+        Flux<Void> tasks = taskExecutor()
+                .flatMap(Function.identity());//runs every Mono<Void> in Flux<Mono<Void>>
+
+        //don't change below this line
+        StepVerifier.create(tasks)
+                    .verifyComplete();
+
+        Assertions.assertEquals(taskCounter.get(), 10);
+    }
+
+    /**
+     * `streamingService()` opens a connection to the data provider.
+     * Once connection is established you will be able to collect messages from stream.
+     *
+     * Establish connection and get all messages from data provider stream!
+     */
+    @Test
+    public void streaming_service() {
+        Flux<Message> messageFlux = streamingService()
+                .flatMapMany(Function.identity()); //runs flux in Mono<Flux<Message>>
+
+        //don't change below this line
+        StepVerifier.create(messageFlux)
+                    .expectNextCount(10)
+                    .verifyComplete();
+    }
+
+
+    /**
+     * Join results from services `numberService1()` and `numberService2()` end-to-end.
+     * First `numberService1` emits elements and then `numberService2`. (no interleaving)
+     *
+     * Bonus: There are two ways to do this, check out both!
+     */
+    @Test
+    public void i_am_rubber_you_are_glue() {
+        Flux<Integer> numbers = numberService1().concatWith(numberService2());
+
+        //don't change below this line
+        StepVerifier.create(numbers)
+                    .expectNext(1, 2, 3, 4, 5, 6, 7)
+                    .verifyComplete();
+    }
+
+    /**
+     * Similar to previous task:
+     *
+     * `taskExecutor()` returns tasks that should execute important work.
+     * Get all the tasks and execute each of them.
+     *
+     * Instead of flatMap() use concatMap() operator.
+     *
+     * Answer:
+     * - What is difference between concatMap() and flatMap()?
+     * - What is difference between concatMap() and flatMapSequential()?
+     * - Why doesn't Mono have concatMap() operator?
+     */
+    @Test
+    public void task_executor_again() {
+        Flux<Void> tasks = taskExecutor().concatMap(Function.identity());
+
+        //don't change below this line
+        StepVerifier.create(tasks)
+                    .verifyComplete();
+
+        Assertions.assertEquals(taskCounter.get(), 10);
+    }
+
+    /**
+     * You are writing software for broker house. You can retrieve current stock prices by calling either
+     * `getStocksGrpc()` or `getStocksRest()`.
+     * Since goal is the best response time, invoke both services but use result only from the one that responds first.
+     */
+    @Test
+    public void need_for_speed() {
+        Flux<String> stonks = Flux.firstWithSignal(getStocksGrpc(), getStocksRest());
+
+        //don't change below this line
+        StepVerifier.create(stonks)
+                    .expectNextCount(5)
+                    .verifyComplete();
+    }
+
+    /**
+     * As part of your job as software engineer for broker house, you have also introduced quick local cache to retrieve
+     * stocks from. But cache may not be formed yet or is empty. If cache is empty, switch to a live source:
+     * `getStocksRest()`.
+     */
+    @Test
+    public void plan_b() {
+        Flux<String> stonks =
+//                Flux.firstWithValue(getStocksLocalCache(), getStocksRest());
+                getStocksLocalCache().switchIfEmpty(getStocksRest());
+        //don't change below this line
+        StepVerifier.create(stonks)
+                    .expectNextCount(6)
+                    .verifyComplete();
+
+        Assertions.assertTrue(localCacheCalled.get());
+    }
+
+    /**
+     * You are checking mail in your mailboxes. Check first mailbox, and if first message contains spam immediately
+     * switch to a second mailbox. Otherwise, read all messages from first mailbox.
+     */
+    @Test
+    public void mail_box_switcher() {
+        Flux<Message> myMail = mailBoxPrimary()
+                .switchOnFirst((first, mailBoxPrimary) -> {
+                    if (first.get().metaData.contains("spam")) {
+                        return mailBoxSecondary();
+                    } else {
+                        return mailBoxPrimary;
+                    }
+        });
+
+        //don't change below this line
+        StepVerifier.create(myMail)
+                    .expectNextMatches(m -> !m.metaData.equals("spam"))
+                    .expectNextMatches(m -> !m.metaData.equals("spam"))
+                    .verifyComplete();
+
+        Assertions.assertEquals(1, consumedSpamCounter.get());
+    }
+
+    /**
+     * You are implementing instant search for software company.
+     * When user types in a text box results should appear in near real-time with each keystroke.
+     *
+     * Call `autoComplete()` function for each user input
+     * but if newer input arrives, cancel previous `autoComplete()` call and call it for latest input.
+     */
+    @Test
+    public void instant_search() {
+        autoComplete(null);
+        Flux<String> suggestions = userSearchInput()
+                .switchMap(this::autoComplete);
+
+        //don't change below this line
+        StepVerifier.create(suggestions)
+                    .expectNext("reactor project", "reactive project")
+                    .verifyComplete();
+    }
+
+
+    /**
+     * Code should work, but it should also be easy to read and understand.
+     * Orchestrate file writing operations in a self-explanatory way using operators like `when`,`and`,`then`...
+     * If all operations have been executed successfully return boolean value `true`.
+     */
+    @Test
+    public void prettify() {
+        Mono<Boolean> successful = Mono
+                .when(openFile())
+                .then(writeToFile("0x3522285912341"))
+                .then(closeFile())
+                .thenReturn(true);
+
+        //don't change below this line
+        StepVerifier.create(successful)
+                    .expectNext(true)
+                    .verifyComplete();
+
+        Assertions.assertTrue(fileOpened.get());
+        Assertions.assertTrue(writtenToFile.get());
+        Assertions.assertTrue(fileClosed.get());
+    }
+
+    /**
+     * Before reading from a file we need to open file first.
+     */
+    @Test
+    public void one_to_n() {
+        Flux<String> fileLines = openFile().thenMany(readFile());
+
+        StepVerifier.create(fileLines)
+                    .expectNext("0x1", "0x2", "0x3")
+                    .verifyComplete();
+    }
+
+    /**
+     * Execute all tasks sequentially and after each task have been executed, commit task changes. Don't lose id's of
+     * committed tasks, they are needed to further processing!
+     */
+    @Test
+    public void acid_durability() {
+        Flux<String> committedTasksIds = tasksToExecute()
+                .concatMap(task -> task.flatMap(taskId -> commitTask(taskId)
+                        .thenReturn(taskId)));
+
+        //don't change below this line
+        StepVerifier.create(committedTasksIds)
+                    .expectNext("task#1", "task#2", "task#3")
+                    .verifyComplete();
+
+        Assertions.assertEquals(3, committedTasksCounter.get());
+    }
+
+
+    /**
+     * News have come that Microsoft is buying Blizzard and there will be a major merger.
+     * Merge two companies, so they may still produce titles in individual pace but as a single company.
+     */
+    @Test
+    public void major_merger() {
+        Flux<String> microsoftBlizzardCorp = microsoftTitles().mergeWith(blizzardTitles());
+
+        //don't change below this line
+        StepVerifier.create(microsoftBlizzardCorp)
+                    .expectNext("windows12",
+                                "wow2",
+                                "bing2",
+                                "overwatch3",
+                                "office366",
+                                "warcraft4")
+                    .verifyComplete();
+    }
+
+
+    /**
+     * Your job is to produce cars. To produce car you need chassis and engine that are produced by a different
+     * manufacturer. You need both parts before you can produce a car.
+     * Also, engine factory is located further away and engines are more complicated to produce, therefore it will be
+     * slower part producer.
+     * After both parts arrive connect them to a car.
+     */
+    @Test
+    public void car_factory() {
+        Flux<Car> producedCars = carChassisProducer().zipWith(carEngineProducer(), Car::new);
+        carChassisProducer();
+        carEngineProducer();
+
+        //don't change below this line
+        StepVerifier.create(producedCars)
+                    .recordWith(ConcurrentLinkedDeque::new)
+                    .expectNextCount(3)
+                    .expectRecordedMatches(cars -> cars.stream()
+                                                       .allMatch(car -> Objects.equals(car.chassis.getSeqNum(),
+                                                                                       car.engine.getSeqNum())))
+                    .verifyComplete();
+    }
+
+    /**
+     * When `chooseSource()` method is used, based on current value of sourceRef, decide which source should be used.
+     */
+
+    //only read from sourceRef
+    AtomicReference<String> sourceRef = new AtomicReference<>("X");
+
+    public Mono<String> chooseSource() {
+        return Mono.defer(() -> {
+            if (sourceRef.get().equals("A")) {
+                return sourceA();
+            } else if (sourceRef.get().equals("B")) {
+                return sourceB();
+            } else return Mono.empty();
+        }); //otherwise, return empty
+    }
+
+    @Test
+    public void deterministic() {
+        //don't change below this line
+        Mono<String> source = chooseSource();
+
+        sourceRef.set("A");
+        StepVerifier.create(source)
+                    .expectNext("A")
+                    .verifyComplete();
+
+        sourceRef.set("B");
+        StepVerifier.create(source)
+                    .expectNext("B")
+                    .verifyComplete();
+    }
+
+    /**
+     * Sometimes you need to clean up after your self.
+     * Open a connection to a streaming service and after all elements have been consumed,
+     * close connection (invoke closeConnection()), without blocking.
+     *
+     * This may look easy...
+     */
+    @Test
+    public void cleanup() {
+
+        Flux<String> stream = Flux.usingWhen(
+                StreamingConnection.startStreaming(), //supplies Flux from Mono
+                n -> n,
+                tr -> StreamingConnection.closeConnection());//cleanup after complete
+
+        //don't change below this line
+        StepVerifier.create(stream)
+                    .then(()-> Assertions.assertTrue(StreamingConnection.isOpen.get()))
+                    .expectNextCount(20)
+                    .verifyComplete();
+        Assertions.assertTrue(StreamingConnection.cleanedUp.get());
+    }
+}
